bb3d9b5bf9a7d4139b70823b2cb9dbcf03874ac4
qnwfs <starik,serg123@gmail.com>
2021-04-18 16:17:28 +1000

    my 2nd solution on this task

--- SortArrayByParity.py
+++ SortArrayByParity.py
@@ -1,17 +1,6 @@
 def SortArrayByParity(A):
-    odd_list = list()
-    even_list = list()
-    final_list = list()
-    for i in A:
-        if i % 2 == 0:
-            even_list.append(i)
-        else:
-            odd_list.append(i)
-    for i in even_list:
-        final_list.append(i)
-    for j in odd_list:
-        final_list.append(j)
-    return final_list
+    A.sort(key=lambda x: x % 2)
+    return A
 
 
 
