f198563f74e88f598fcd201c52f013072f97c30c
qnwfs <starik,serg123@gmail.com>
2021-04-18 16:35:52 +1000

    my 1st solution on this task

---
+++ SumOfUniqueElements.py
@@ -0,0 +1,12 @@
+from collections import Counter
+def sumOfUnique(nums):
+    a = Counter(nums)
+    x = a.most_common()
+    sum = 0
+    for i in x:
+        if i[1] == 1:
+            sum += i[0]
+    return sum
+
+nums = [1,1,1,1,1]
+print(sumOfUnique(nums))
