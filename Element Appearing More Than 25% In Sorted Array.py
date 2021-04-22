7d5316dca6ba8761c34265e2edc017091dc6aaa8
qnwfs <starik.serg123@gmail.com>
2021-04-23 00:12:02 +1000

    Faster but greedy

--- Element Appearing More Than 25% In Sorted Array.py
+++ Element Appearing More Than 25% In Sorted Array.py
@@ -1,14 +1,8 @@
+import collections
+
 def findSpecialInteger(arr: list):
-    maxc = 0
-    max = 0
-    for i in arr:
-        if arr.count(i) >= maxc:
-            maxc = arr.count(i)
-            max = i
-    if maxc >= len(arr)/4:
-        return max
-    else:
-        return None
+    x = collections.Counter(arr)
+    return x.most_common()[0][0]
 
 arr = [6700,8858,8858,8858,8858]
 print(findSpecialInteger(arr))
