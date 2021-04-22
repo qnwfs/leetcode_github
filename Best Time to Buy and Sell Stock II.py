999dca6b12239005f219c32740b72c509c802c65
qnwfs <starik.serg123@gmail.com>
2021-04-22 23:25:54 +1000

    Runtime: 60 ms, faster than 71.38% of Python3 online submissions for Best Time to Buy and Sell Stock II. Memory Usage: 15.2 MB, less than 21.74% of Python3 online submissions for Best Time to Buy and Sell Stock II.

---
+++ Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,16 @@
+def max_profit(prices: list):
+    profit = 0
+    for i in range(len(prices)):
+        if i + 1 >= len(prices):
+            break
+        else:
+            if prices[i] < prices[i+1]:
+                profit += prices[i+1]-prices[i]
+    return profit
+
+
+prices = [7,6,4,3,1]
+print(max_profit(prices))
+
+
+
