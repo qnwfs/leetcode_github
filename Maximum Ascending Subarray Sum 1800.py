62c606768587a056625e62aaf1aff7e320c88af2
qnwfs <starik.serg123@gmail.com>
2021-04-25 21:58:24 +1000

    Runtime: 28 ms, faster than 95.10% of Python3 online submissions for Maximum Ascending Subarray Sum. Memory Usage: 14.2 MB, less than 47.55% of Python3 online submissions for Maximum Ascending Subarray Sum.

--- Maximum Ascending Subarray Sum 1800.py
+++ Maximum Ascending Subarray Sum 1800.py
@@ -1,23 +1,14 @@
 def maxAscendingSum(nums: list):
-    total = 0
-    lisp = list()
-    if len(nums) == 1:
-        return nums[0]
+    sums = []
+    total = nums[0]
     for i in range(len(nums)-1):
-        if nums[i] < nums[i+1]:
-            total += nums[i]
+        if nums[i+1] > nums[i]:
+            total += nums[i+1]
         else:
-            total += nums[i]
-            lisp.append(total)
-            total = 0
-    if nums[len(nums) - 1] > nums[len(nums) - 2]:
-        total += nums[len(nums) - 1]
-        lisp.append(total)
-    else:
-        lisp.append(total)
-    lisp.sort(reverse=True)
-    return lisp[0]
-
+            sums.append(total)
+            total = nums[i+1]
+    sums.append(total)
+    return max(sums)
 
 nums = [100,10,1]
 print(maxAscendingSum(nums))
