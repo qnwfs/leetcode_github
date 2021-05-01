33b653123d6e00c39719012cfd2475eb5f189913
qnwfs <starik.serg123@gmail.com>
2021-05-01 12:27:50 +1000

    Runtime: 48 ms, faster than 42.56% of Python3 online submissions for Find Peak Element. Memory Usage: 14.2 MB, less than 90.09% of Python3 online submissions for Find Peak Element.

---
+++ Find Peak Element 162.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                return i
+        else:
+            return len(nums)-1
