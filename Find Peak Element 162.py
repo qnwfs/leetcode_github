ef6c84e52f7d16def452d350c7a802d86664054c
qnwfs <starik.serg123@gmail.com>
2021-05-01 13:11:35 +1000

    solution using binary search

--- Find Peak Element 162.py
+++ Find Peak Element 162.py
@@ -1,7 +1,14 @@
-class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        for i in range(len(nums)-1):
-            if nums[i] > nums[i+1]:
-                return i
+def findPeakElement(nums: list):
+    left = 0
+    right = len(nums)-1
+    while left < right:
+        mid = (right+left)//2
+        if nums[mid] > nums[mid+1]:
+            right = mid
         else:
-            return len(nums)-1
+            left = mid + 1
+    return left
+
+
+nums = [1,2,3,1,2,1]
+print(findPeakElement(nums))
