f842951c35b7f4621216b0ee1b5377ae7d679232
qnwfs <starik.serg123@gmail.com>
2021-07-07 16:25:33 +1000

    for better understanding

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -5,6 +5,10 @@ def moveZeroes(nums) -> None:
             nums[i], nums[zero] = nums[zero], nums[i]
             zero += 1
     return nums
+    """
+    Берём индекс нуля и как только попадаем на ненулевое число - меняем местами с нулём.
+    zero в счетчике прибавляется если число не ноль. Таким образом мы тащим все нули вправо
+    """
 
-nums = [0, 1, 0, 3, 12]
+nums = [1, 0, 0, 2, 0]
 print(moveZeroes(nums))
